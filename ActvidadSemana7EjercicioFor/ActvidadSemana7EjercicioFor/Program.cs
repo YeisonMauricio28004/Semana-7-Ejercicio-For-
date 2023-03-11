dc0c76df28c591157e3af7d02a3652174f7ee5af
YeisonMauricio28004 <116232628+YeisonMauricio28004@users.noreply.github.com>
2023-03-11 10:31:49 -0600

    Ejercicio de la Semana 7 sobre los Bucles For Ya Terminado

---
+++ ActvidadSemana7EjercicioFor/ActvidadSemana7EjercicioFor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int altura;
+
+        Console.Write("Introduce la altura del triángulo: ");
+        altura = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= altura; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
+
+
